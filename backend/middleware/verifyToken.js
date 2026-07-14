09767f809fbfe9179d4da6463ae5acb730893752
Naveen EK <naveenerukattil@gmail.com>
2026-07-14 20:22:03 +0530

    update verifyToken

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import db from "../config/db.js";
 
 export const verifyToken = (req, res, next) => {
   try {
@@ -13,6 +14,25 @@ export const verifyToken = (req, res, next) => {
     const decoded = jwt.verify(
       token, process.env.JWT_SECRET
     );
+
+    const[rows]=await db.query(
+      `select employee_status
+      from employees
+      where employee_id=?`,
+      [decoded.employeeId]
+    );
+    if(rows.length===0){
+      return res.status(401).json({
+        success:false,
+        message:"Employee not found"
+      });
+    }
+    if(rows[0].employee_status !== "active"){
+      return res.status(403).json({
+        success:false,
+        message:"Account is inactive"
+      });
+    }
     req.user = decoded
 
     next();
