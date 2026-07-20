50f07e374362a4ea257cd32269b242551f40f9c1
Naveen EK <naveenerukattil@gmail.com>
2026-07-20 21:15:56 +0530

    dio client structure completed

--- frontend/lib/core/network/interceptors/auth_interceptor.dart
+++ frontend/lib/core/network/interceptors/auth_interceptor.dart
@@ -1,1 +1,14 @@
 // automatically attach the jwt token to the request so that the backend can access the jwt token,without this we need to the attach jwt to all the request individually  
+import 'package:dio/dio.dart';
+
+class AuthInterceptor extends Interceptor{
+  //we extends Interceptor to this class beacuse the dio is designed to work with the object of type Interceptor 
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  )async{
+
+    handler.next(options);
+  }
+}
