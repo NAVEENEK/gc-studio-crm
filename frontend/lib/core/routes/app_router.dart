4c0f0dcd8c1053635c668aa2e3950af4519fdd83
Naveen EK <naveenerukattil@gmail.com>
2026-07-24 09:39:12 +0530

    working login screen

--- frontend/lib/core/routes/app_router.dart
+++ frontend/lib/core/routes/app_router.dart
@@ -1,23 +1,30 @@
-import 'package:flutter/material.dart';
+
 import 'package:go_router/go_router.dart';
 import 'package:frontend/core/routes/app_routes.dart';
-import 'package:frontend/features/auth/ui/widgets/login_form.dart';
+import 'package:frontend/features/auth/ui/login_screen.dart';
 import 'package:frontend/features/auth/ui/employee_dashboard.dart';
+import 'package:frontend/features/auth/ui/agency_dashboard.dart';
 
 class AppRouter {
   AppRouter._();
 
   static final router=GoRouter(
+    initialLocation: AppRoutes.login,
     routes: [
       GoRoute(
         path: AppRoutes.login,
-        builder: (context,State)=> const LoginForm(),
+        builder: (context,State)=> const LoginScreen(),
         ),
 
         GoRoute(
           path: AppRoutes.employeeDashboard,
           builder: (context,state)=>const EmployeeDashboard(),
-          )
+          ),
+
+          GoRoute(
+            path: AppRoutes.agencyDashboard,
+            builder: (context,state)=> const AgencyDashboard(),
+            )
     ]
     );
 }
---
+++ frontend/lib/features/auth/ui/agency_dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AgencyDashboard extends StatelessWidget {
+  const AgencyDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
